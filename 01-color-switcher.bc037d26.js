b1403c7aad96d0ad72e19b702f78197b5cd23c69
zaharoid <119331894+zaharoid@users.noreply.github.com>
2023-05-31 12:33:54 +0000

    Deploying to gh-pages from @ zaharoid/goit-js-hw-09@c5c212d24aa700d60a30e4e562226fc2e3fc2c67 🚀

---
+++ 01-color-switcher.bc037d26.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")},n=null;t.stopBtn.disabled=!0,t.startBtn.addEventListener("click",(function(){n=setInterval((function(){return document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.startBtn.disabled=!0,t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(function(){clearInterval(n),t.startBtn.disabled=!1,t.stopBtn.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.bc037d26.js.map
